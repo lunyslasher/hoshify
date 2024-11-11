cdc1cc21a5c008b7c25b18848ce0731db0fe745d
lunyslasher <r4mzya@gmail.com>
2024-11-11 21:56:34 +0600

    added cookie parser

--- app/services/auth/src/index.ts
+++ app/services/auth/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import connectToDatabase from "./database";
 import routes from "./routes";
+import cookieParser from 'cookie-parser';
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
@@ -9,6 +10,7 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 app.use(routes);
 
 const start = async () => {
