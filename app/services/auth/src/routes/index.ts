e228c4f0006679cbb343a677ae847c9d1d59cb1c
lunyslasher <r4mzya@gmail.com>
2024-11-11 21:35:12 +0600

    added auth routes

--- app/services/auth/src/routes/index.ts
+++ app/services/auth/src/routes/index.ts
@@ -1,9 +1,13 @@
 import {Router} from 'express';
+import {login, register} from "../controllers/auth.controller";
 
 const router = Router();
 
 router.get(`/test`, async (req, res) => {
     res.status(200).json({message: `Working`});
 })
+
+router.post(`/login`, login);
+router.post(`/register`, register);
 
 export default router;
