d16d8a3218772bc2b4ec355603a8b1be5525d50e
Luny <r4mzya@gmail.com>
2025-03-11 23:54:10 +0600

    cookie parser

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@ import express from "express";
 import {config} from "dotenv";
 import sequelize from "./database";
 import routes from "./routes";
+import cookieParser from 'cookie-parser';
 
 config();
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 app.use(routes);
 
 const start = async() => {
